70d2182b4cadf287cb8b14e289398d4045a4c575
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-08 08:26:42 -0400

    Finish to choose the assets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import pokeball from './assets/img/pokeball-logo.png';
 
 const pokeballLogo = document.getElementById('pokeballLogo');
 
+// add icon to the page
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = pokeball;
+document.head.appendChild(link);
+
 const img = document.createElement('img');
 img.src = pokeball;
 img.alt = 'Pokeball';
