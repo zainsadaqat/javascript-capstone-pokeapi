61c99517ad26bbaf484569612890990200dbb571
Zain Sadaqat <zainsadaqat@192.168.10.8>
2021-09-09 23:05:44 +0500

    Create method to get pokemons

--- src/get-pokemons.js
+++ src/get-pokemons.js
@@ -1,4 +1,4 @@
-const pokemon = async () => {
+  const pokemon = async () => {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
   const result = response.json();
   return result;
