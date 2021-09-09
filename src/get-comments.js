9e069fb3b494eeafd97aef3cb7e2f9b63a9f2e70
Zain Sadaqat <zainsadaqat@192.168.10.8>
2021-09-09 23:07:20 +0500

    Create and implement get all comments

---
+++ src/get-comments.js
@@ -0,0 +1,13 @@
+const ID = 'sOH39XWa9gMFVPLXHU2G';
+const URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${ID}/comments`;
+
+const getComments = async (itemId) => {
+  const fetchComments = `${URL}?item_id=item${itemId}`;
+  const response = await fetch(fetchComments, {
+    method: 'GET',
+  });
+  const list = await response.json();
+  return list;
+};
+
+export default getComments;
